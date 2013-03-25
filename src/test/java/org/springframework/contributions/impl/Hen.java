63ba20ddafc5844de1f13902d5c5ef1a2ddf22b0
christian.koeberl@gmail.com <christian.koeberl@gmail.com@fbb20255-fc04-ba64-9e8f-005440561780>
2013-03-25 17:52:27 +0000

    added checkstyle config

--- src/test/java/org/springframework/contributions/impl/Hen.java
+++ src/test/java/org/springframework/contributions/impl/Hen.java
@@ -2,31 +2,34 @@ package org.springframework.contributions.impl;
 
 import java.util.List;
 
-public class Hen {
+public class Hen
+{
+    private final List<Egg> eggs;
+    private final String name;
 
-	private final List<Egg> eggs;
-	private final String name;
+    public Hen(List<Egg> eggs, String name)
+    {
+        super();
 
-	public Hen(List<Egg> eggs, String name) {
-		super();
-		
-		this.eggs = eggs;
-		this.name = name;
-	}
+        this.eggs = eggs;
+        this.name = name;
+    }
 
-	public List<Egg> getEggs() {
-		return eggs;
-	}
-	
-	public String getName() {
-		return name;
-	}
+    public List<Egg> getEggs()
+    {
+        return eggs;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return String.format("%s has %d eggs: %s", getName(), getEggs().size(), getEggs());
-	}
-	
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String toString()
+    {
+        return String.format("%s has %d eggs: %s", getName(), getEggs().size(), getEggs());
+    }
 }
