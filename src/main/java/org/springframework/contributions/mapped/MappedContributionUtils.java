05c9ca3a0af20bb119410561df37067f6996b13e
christian.koeberl@gmail.com <christian.koeberl@gmail.com@fbb20255-fc04-ba64-9e8f-005440561780>
2013-03-25 15:49:23 +0000

    source format

--- src/main/java/org/springframework/contributions/mapped/MappedContributionUtils.java
+++ src/main/java/org/springframework/contributions/mapped/MappedContributionUtils.java
@@ -11,24 +11,23 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 
 public class MappedContributionUtils
 {
-	public static BeanDefinition addContribution(String contributionName, Map map, BeanDefinitionRegistry registry)
-	{
-		final String beanName = MAPPED_CONTRIBUTION_PREFIX + contributionName;
+    public static BeanDefinition addContribution(String contributionName, Map map, BeanDefinitionRegistry registry)
+    {
+        final String beanName = MAPPED_CONTRIBUTION_PREFIX + contributionName;
         BeanDefinition beanDefinition;
         if (registry.containsBeanDefinition(beanName))
         {
             beanDefinition = registry.getBeanDefinition(beanName);
             beanDefinition.getPropertyValues().addPropertyValue("sourceMap", map);
-
         }
         else
         {
-        	BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(MapFactoryBean.class);
+            BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(MapFactoryBean.class);
             builder.addPropertyValue("sourceMap", map);
             beanDefinition = builder.getBeanDefinition();
             registry.registerBeanDefinition(beanName, beanDefinition);
         }
 
         return beanDefinition;
-	}
+    }
 }
